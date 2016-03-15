764410e27a1bff5f377d2231881d8db917f560f3
Mykhailo Danylov <mike.danylov@gmail.com>
2016-03-15 21:04:53 +0200

    Add Places schema

---
+++ places_schema.js
@@ -0,0 +1,46 @@
+Places = new Mongo.Collection('places');
+
+// schema for songslist collection
+PlacesSchema = new SimpleSchema({
+	name: {
+		type: String,
+		label: 'Name',
+		max: 64
+	},
+	address: {
+		type: String,
+		label: 'Address',
+		max: 256
+	},
+	popularity: {
+		type: Number,
+		label: 'Total reservation counter',
+		defaultValue: 0,
+		min: 0
+	},
+	location: {
+		type: String,
+		label: 'Song duration'
+	},
+	createdAt: {
+		type: Date,
+		label: 'Created at',
+		autoValue: function() {
+			if (this.isInsert) {
+				return new Date();
+			}
+		}
+	},
+	updatedAt: {
+        type: Date,
+        label: 'Updated at',
+        optional: true, // not sure about this, might not work when updating
+        autoValue: function () {
+            if (this.isUpdate) {
+                return new Date();
+            }
+        }
+    }
+});
+
+Places.attachSchema(PlacesSchema);
